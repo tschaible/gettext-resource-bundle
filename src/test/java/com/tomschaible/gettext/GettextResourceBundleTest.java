ec48fda036ad687647f2451ac4adaee2aa7440c2
Tom Schaible <tschaible@gmail.com>
2012-10-20 21:28:03 -0400

    Initial PO file support for comments, whitespace, msgid, msgstr

---
+++ src/test/java/com/tomschaible/gettext/GettextResourceBundleTest.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2012 Tom Schaible
+ * See the file license.txt for copying permission.
+ */
+package com.tomschaible.gettext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+/**
+ * @author Tom Schaible
+ * 
+ *         Unit tests for GettextResourceBundle
+ */
+public class GettextResourceBundleTest {
+
+	@Test
+	public void testNormalPOFile()
+	{
+		GettextResourceBundle rb = new GettextResourceBundle(Thread.currentThread().getContextClassLoader().getResourceAsStream("test.po"));
+		
+		assertEquals(rb.getString("key1"),"value1");
+		assertEquals(rb.getString("key2"),"value2");
+		assertFalse(rb.containsKey("key3\\nwith some other stuff"));		
+		assertEquals(rb.getString("key3\nwith some other stuff"),"value3\nwith some different stuff");
+		
+		
+	}
+}
