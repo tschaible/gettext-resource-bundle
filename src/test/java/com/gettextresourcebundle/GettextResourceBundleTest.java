f3b926cb7c621c985669b0f3fab457b9bcb85869
Tom Schaible <tschaible@gmail.com>
2013-06-29 15:06:51 -0400

    Creating unit tests that test the functionality of escaping special characters

--- src/test/java/com/gettextresourcebundle/GettextResourceBundleTest.java
+++ src/test/java/com/gettextresourcebundle/GettextResourceBundleTest.java
@@ -6,6 +6,7 @@ package com.gettextresourcebundle;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -18,16 +19,36 @@ import com.gettextresourcebundle.GettextResourceBundle;
  */
 public class GettextResourceBundleTest {
 
+	/**
+	 * test a PO file containing normal text
+	 * specifically test
+	 *  - a normal key
+	 *  - keys spread across multiple values
+	 *  - keys containing the newline string
+	 */
 	@Test
 	public void testNormalPOFile()
 	{
 		GettextResourceBundle rb = new GettextResourceBundle(Thread.currentThread().getContextClassLoader().getResourceAsStream("test.po"));
 		
-		assertEquals(rb.getString("key1"),"value1");
-		assertEquals(rb.getString("key2"),"value2");
-		assertFalse(rb.containsKey("key3\\nwith some other stuff"));		
-		assertEquals(rb.getString("key3\nwith some other stuff"),"value3\nwith some different stuff");
+		assertEquals("Testing fetch of normal key/value pair",rb.getString("key1"),"value1");
+		assertEquals("Testing fetch of key/values extended over multiple lines", rb.getString("key2"),"value2");
+		assertTrue("Testing containsKey returns true for present keys", rb.containsKey("key3\nwith some other stuff"));
+		assertFalse("Testing containsKey returns false for missing keys", rb.containsKey("key3\\nwith some other stuff"));		
+		assertEquals("Testing fetch of key/values extended over multiple lines", rb.getString("key3\nwith some other stuff"),"value3\nwith some different stuff");
 		
 		
+	}
+	
+	@Test
+	public void testSpecialCharactersPOFile()
+	{
+		GettextResourceBundle rb = new GettextResourceBundle(Thread.currentThread().getContextClassLoader().getResourceAsStream("special-chars.po"));
+		
+		assertEquals("Testing escaped characters are properly parsed", rb.getString("allspecialchars"),"\t\b\n\r\f\'\"\\");
+		assertEquals("Testing escaping of escaped characters are properly parsed", rb.getString("escapedspecialchars"),"\\t\\b\\n\\r\\f\\'\\\"");
+		assertEquals("Testing escaped unicode characters are properly parsed", rb.getString("escapedunicode"),"π");
+		assertEquals("Testing escaped octal characters are properly parsed", rb.getString("escapedoctal"),"aBc^");
+
 	}
 }
